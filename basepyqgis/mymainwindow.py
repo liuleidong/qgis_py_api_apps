0d6bd0f360c3a626e38d05d8bdb8b00876f8cd40
liuleidong <liuleidong@163.com>
2024-05-10 11:43:40 +0800

    增加maptool

--- basepyqgis/mymainwindow.py
+++ basepyqgis/mymainwindow.py
@@ -1,8 +1,9 @@
-from PyQt5.QtWidgets import QVBoxLayout,QHBoxLayout
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QAction
 
 from qgis.PyQt.QtWidgets import QMainWindow
 from qgis.core import QgsProject,QgsLayerTreeModel,QgsVectorLayer
-from qgis.gui import QgsLayerTreeView,QgsMapCanvas,QgsLayerTreeMapCanvasBridge
+from qgis.gui import QgsLayerTreeView,QgsMapCanvas,QgsLayerTreeMapCanvasBridge,QgsMapToolPan,QgsMapToolZoom
 
 from ui.MainWindow import Ui_MainWindow
 
@@ -18,6 +19,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         vl.addWidget(self.gsLayerTreeView)
         # 初始化mapCanvas
         self.gsMapCanvas = QgsMapCanvas(self)
+        # TODO 设置destcrs 和 extent
         hl = QHBoxLayout(self.mapcanvasWidget)
         hl.setContentsMargins(0,0,0,0)
         hl.addWidget(self.gsMapCanvas)
@@ -33,10 +35,32 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.gsLayerTreeView.setModel(self.gsLayerTreeModel)
         # synchronise the loaded project with the canvas
         self.gsLayerTreeBridge = QgsLayerTreeMapCanvasBridge(QgsProject.instance().layerTreeRoot(), self.gsMapCanvas)
-
+        # 加载测试图层
         vlayer = QgsVectorLayer("../python_cookbook/airports.shp", "airports", "ogr")
         if not vlayer:
             self.statusbar.showMessage("Layer failed to load!")
         else:
             self.statusbar.showMessage("Layer load Done!")
             QgsProject.instance().addMapLayer(vlayer)
+        # 添加三个maptool
+        self.gsMapToolPan = QgsMapToolPan(self.gsMapCanvas)
+        self.gsMapToolZoomIn = QgsMapToolZoom(self.gsMapCanvas,False)
+        self.gsMapToolZoomOut = QgsMapToolZoom(self.gsMapCanvas,True)
+        self.gsMapCanvas.setMapTool(self.gsMapToolPan)
+        # 添加ToolBar用于切换Maptool
+        tb = self.addToolBar('MapTools')
+        self.actionPan = QAction(QIcon(':/images/mActionPan.png'),'Pan',self)
+        self.actionZoomIn = QAction(QIcon(':/images/mActionZoomIn.png'),'ZoomIn',self)
+        self.actionZoomOut = QAction(QIcon(':/images/mActionZoomOut.png'),'ZoomOut',self)
+        tb.addAction(self.actionPan)
+        tb.addAction(self.actionZoomIn)
+        tb.addAction(self.actionZoomOut)
+        tb.actionTriggered[QAction].connect(self.toolbtnpressed)
+
+    def toolbtnpressed(self, a):
+        if self.actionPan == a:
+            self.gsMapCanvas.setMapTool(self.gsMapToolPan)
+        elif self.actionZoomIn == a:
+            self.gsMapCanvas.setMapTool(self.gsMapToolZoomIn)
+        elif self.actionZoomOut == a:
+            self.gsMapCanvas.setMapTool(self.gsMapToolZoomOut)
